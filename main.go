95a7f37b09fe85db5d220acc2c76d1d53ae4fbe1
Marek Tichy <marek@gn.apc.org>
2022-02-11 08:44:47 +0100

    Added InfluxDB support

--- main.go
+++ main.go
@@ -5,10 +5,19 @@ import (
 	mr "math/rand"
 	"time"
 	tl "trits/tritslab"
+
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/influxdata/influxdb-client-go/v2/api"
 )
 
 func main() {
+	// Initialize the random number generator
 	mr.Seed(time.Now().UnixNano())
+	// Initialize INFLUX connection
+	client := influxdb2.NewClient("http://"+tl.INFLUX_HOST+":"+tl.INFLUX_PORT, tl.INFLUX_API_KEY)
+	influxWriteAPI := client.WriteAPI("Trits", "tritslab")
+
+	defer client.Close()
 	/*
 		// open log file
 		logFile, err := os.OpenFile(tl.LOG_FILE, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
@@ -18,20 +27,30 @@ func main() {
 		defer logFile.Close()
 	*/
 	fmt.Print("ROUND,Bank,Zion," + tl.LogPlayersHeaders())
-	playsome()
+	playsome(influxWriteAPI)
 }
 
-func playsome() {
+func playsome(infWAPI api.WriteAPI) {
 	c := tl.NewTritsCroupier(140000, 10000, 10000)
 	var j = 0
 	var cont = true
 	for cont && j < 1000000 {
 		//tl.L(tl.LOG_DEBUG, "Round:", j)
-		fmt.Print(fmt.Sprint(j, ",", c.Banker.Tell(tl.NewTritsAddress(tl.BankAddr)), ",", tl.LogZionTotal(c.Banker), ",", tl.LogPlayersBalances(c.Banker)))
+		bank_balance := c.Banker.Tell(tl.NewTritsAddress(tl.BankAddr))
+		zion_balance := tl.LogZionTotal(c.Banker)
+		now := time.Unix(0, time.Now().UnixNano())
+		p := influxdb2.NewPoint("balance",
+			map[string]string{"unit": "balance"},
+			map[string]interface{}{"bank": bank_balance, "zion": zion_balance},
+			now,
+		)
+		infWAPI.WritePoint(p)
+		fmt.Print(fmt.Sprint(j, ",", bank_balance, ",", zion_balance, ",", tl.LogPlayersBalances(c.Banker)))
 		j++
 		cont = c.AskAround()
 
 	}
+	infWAPI.Flush()
 	fmt.Println(c.Banker.DumpBank())
 	/*
 		res := c.Banker.Tell(NewTritsAddress(BankAddr))
--- tritslab/config.go
+++ tritslab/config.go
@@ -10,6 +10,10 @@ const BONUS_HIGH = 4
 const ZION_RECHARGE_AT = 0.9 // Bank helps out zion players who go under this treshold
 const LOG_LEVEL = LOG_DEBUG  // LOG_DEBUG<LOG_INFO < LOG_NOT
 const LOG_FILE = "/var/log/trits/tritslab.log"
-const WORLDS_MONEY = 1000000000 // Borrow fund initial amount
-const PROFIT_TRESHOLD = 4       // Times the initial amount
-const LEAVE_GAME_PROB = 50      // 1-100 chance to leave the game if profit is gretter than the PROFIT_TRESHOLD
+const WORLDS_MONEY = 1000000000                                                                                   // Borrow fund initial amount
+const PROFIT_TRESHOLD = 4                                                                                         // Times the initial amount
+const LEAVE_GAME_PROB = 50                                                                                        // 1-100 chance to leave the game if profit is gretter than the PROFIT_TRESHOLD
+const INFLUX_HOST = "127.0.0.1"                                                                                   // Influx DB host
+const INFLUX_PORT = "8086"                                                                                        // Influx DB port
+const INFLUX_API_KEY = "BwRDdZLL2kRaRJvfzjrb-Yw-cOBph2R7JLt5YWS-6BkZp5s8BKucn_bZ4vskHJNI29vrldyGwPn6oU-z2mKfiQ==" // Influx DB API KEY
+const INFLUX_BUCKET = "tritslab"                                                                                  // Influx DB bucket
